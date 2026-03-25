7eb8a06f378c2ad454f9ce263698e32c1550a64b
qwuko <verast2006@gmail.com>
2026-03-26 00:28:13 +0300

    Изначальная версия

--- MusicCollectionApp/MusicCollectionApp/MusicTrack.cs
+++ MusicCollectionApp/MusicCollectionApp/MusicTrack.cs
@@ -9,21 +9,6 @@ public class MusicTrack
 
     public MusicTrack(string artist, string title, string genre, int year)
     {
-        if (string.IsNullOrWhiteSpace(artist))
-        {
-            throw new ArgumentException("Исполнитель не может быть пустым", nameof(artist));
-        }
-
-        if (string.IsNullOrWhiteSpace(title))
-        {
-            throw new ArgumentException("Название трека не может быть пустым", nameof(title));
-        }
-
-        if (year < 1900 || year > DateTime.Now.Year)
-        {
-            throw new ArgumentException($"Год должен быть между 1900 и {DateTime.Now.Year}", nameof(year));
-        }
-
         Artist = artist;
         Title = title;
         Genre = genre;
