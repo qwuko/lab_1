74de405e44de46f4aab222348929ed563c9d9ea6
qwuko <verast2006@gmail.com>
2026-03-25 21:31:36 +0300

    Тесты для класса MusicTrack

---
+++ MusicCollectionApp/MusicCollectionAppTests/MusicTrackTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicCollectionApp;
+
+namespace MusicCollectionAppTests
+{
+    [TestClass]
+    public class MusicTrackTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void YearLessThan1900_IsNotAllowed()
+        {
+            new MusicTrack("Artist", "Title", "Genre", 1899);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void YearGreaterThanCurrent_IsNotAllowed()
+        {
+            new MusicTrack("Artist", "Title", "Genre", DateTime.Now.Year + 1);
+        }
+
+        [TestMethod]
+        public void Year1900_IsAllowed()
+        {
+            int year = 1900;
+
+            var track = new MusicTrack("Artist", "Title", "Genre", year);
+
+            Assert.AreEqual(year, track.Year);
+        }
+
+        [TestMethod]
+        public void YearCurrent_IsAllowed()
+        {
+            int year = DateTime.Now.Year;
+
+            var track = new MusicTrack("Artist", "Title", "Genre", year);
+
+            Assert.AreEqual(year, track.Year);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EmptyArtist_IsNotAllowed()
+        {
+            new MusicTrack("", "Title", "Genre", 2020);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WhitespaceArtist_IsNotAllowed()
+        {
+            new MusicTrack("   ", "Title", "Genre", 2020);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EmptyTitle_IsNotAllowed()
+        {
+            new MusicTrack("Artist", "", "Genre", 2020);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WhitespaceTitle_IsNotAllowed()
+        {
+            new MusicTrack("Artist", "   ", "Genre", 2020);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NegativeYear_IsNotAllowed()
+        {
+            new MusicTrack("Artist", "Title", "Genre", -100);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void YearZero_IsNotAllowed()
+        {
+            new MusicTrack("Artist", "Title", "Genre", 0);
+        }
+    }
+}
---
+++ MusicCollectionApp/MusicCollectionAppTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MusicCollectionAppTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MusicCollectionAppTests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("066b4a5f-a484-4efe-8723-76744605d55f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
